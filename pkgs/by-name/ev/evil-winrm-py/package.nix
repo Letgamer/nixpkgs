65e3efceedac27ccf45d20c870bf068f2eca1a5c
Letgamer <alexstephan005@protonmail.com>
2026-09-19 16:14:01 +0200

    evil-winrm-py: only relax two deps

--- pkgs/by-name/ev/evil-winrm-py/package.nix
+++ pkgs/by-name/ev/evil-winrm-py/package.nix
@@ -20,7 +20,10 @@ python3Packages.buildPythonApplication (finalAttrs: {
     hash = "sha256-xB+JLeh5MUWHd9wWLEVFLkN4QxyRaaa1p6442hsshXc=";
   };
 
-  pythonRelaxDeps = true;
+  pythonRelaxDeps = [
+    "pypsrp"
+    "prompt_toolkit"
+  ];
 
   # Removes the additional binary ewp
   postPatch = ''
